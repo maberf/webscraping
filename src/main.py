45824106b897614fdcc03c00cc794f3c6ddf2a5b
Maber Fernandes <71026050+maberf@users.noreply.github.com>
2020-09-17 12:38:55 -0300

    Draft
    Draft

--- src/main.py
+++ src/main.py
@@ -1,23 +1,26 @@
 # MAIN
 from webtableparser import WebTableParser
 
-'''site1 = WebTableParser('https://www.fundsexplorer.com.br/ranking', 'class', 'table table-hover')
+site1 = WebTableParser('https://www.fundsexplorer.com.br/ranking', 'table table-hover')
 table1 = site1.capture()
+print(type(table1))
 df1 = site1.parse(table1)
 print(df1)
-# id='table-ranking' class='table table-hover'
+# id='table-ranking' class='table table-hover'''
 
-site2 = WebTableParser('https://calculador.com.br/tabela/indice/IGP-M', 'class', 'table table-bordered table-striped table-hover table-fixed mb-0')
+'''site2 = WebTableParser('https://calculador.com.br/tabela/indice/IGP-M', 'table table-bordered table-striped table-hover table-fixed mb-0')
 table2 = site2.capture()
 df2 = site2.parse(table2)
 print(df2)'''
 
-site3 = WebTableParser('https://sidra.ibge.gov.br/home/ipca/brasil', 'id', 'ipca-q1')
+'''site3 = WebTableParser('https://sidra.ibge.gov.br/home/ipca/brasil', 'quadro tabela-sidra')
 table3 = site3.capture()
+print(table3)
 df3 = site3.parse(table3)
 print(df3)
+# class = 'quadro tabela-sidra'  id = ipca-q1'''
 
-'''site4 = WebTableParser('https://pt.wikipedia.org/wiki/Lista_de_capitais_do_Brasil_por_%C3%A1rea', 'class', 'wikitable sortable')
+site4 = WebTableParser('https://pt.wikipedia.org/wiki/Lista_de_capitais_do_Brasil_por_%C3%A1rea', 'wikitable sortable')
 table4 = site4.capture()
 df4 = site4.parse(table4)
-print(df4)'''
+print(df4)
--- src/webtableparser.py
+++ src/webtableparser.py
@@ -6,43 +6,46 @@ from typing import List
 
 class WebTableParser:
 
-    def __init__(self, site, key, spdsheetKey):
+    def __init__(self, site, spdsheetClassName):
+        """objetc to read spreadsheets in websites"""
         self.site = site
-        self.key = key
-        self.spdsheetKey = spdsheetKey
+        self.spdsheetClassName = spdsheetClassName
         pass
 
     def capture(self):
-        # URL request with web browser agent
+        """Function to capture information in website
+        Returns:
+            [type]: [bs4.element.Tag]
+        """
+        # URL request through web browser agent
         siteurl = request.Request(self.site, headers={'User-Agent': 'Mozilla/5.0'})
         page = request.urlopen(siteurl)
         soup = BeautifulSoup(page, 'html5lib')
-        table = soup.find('table', attrs={self.key: self.spdsheetKey})
+        table = soup.find('table', attrs={'class': self.spdsheetClassName})
         return table
 
     def parse(self, table):
-        # atributos da estrutura do dataframe
-        nColunas = 0
-        nLinhas = 0
+        # assembling information to dataframe
+        columns = 0
+        lines = 0
         columnnames: List[str] = []
         #
         for row in table.find_all('tr'):  # 1o rastreamento (estrutura)
             # contar número de linhas e colunas
             td_tags = row.find_all('td')
-            if nColunas == 0:
-                nColunas = len(td_tags)
             if len(td_tags) > 0:
-                nLinhas += 1
+                lines += 1
+                if columns == 0:
+                    columns = len(td_tags)
         #
             # captura o titulo das colunas - se tiver thead/th
             th_tags = row.find_all('th')
             if len(th_tags) > 0 and len(columnnames) == 0:
                 for th in th_tags:
                     columnnames.append(th.get_text())
-        # print(nLinhas, nColunas, columnnames)  # OK?
         #
         # criando o dataframe com o pandas para armazenar os dados
-        df = pd.DataFrame(columns=columnnames, index=range(0, nLinhas))
+        df = pd.DataFrame(columns=columnnames, index=range(0, lines))
         row_marker = 0
         # percorre as celulas para extrair o texto e gravar no dataframe
         for row in table.find_all('tr'):  # 2o rastreamento (carregamento df)
