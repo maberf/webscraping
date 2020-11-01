d8003dc257d8affb5179de83c6fba4a9cffc7973
Maber Fernandes <71026050+maberf@users.noreply.github.com>
2020-11-01 19:47:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@
 from src.webtableparser import WebTableParser
 #
 # Sites 1 e 2 for testing (remove comments)
-site1 = WebTableParser()
+'''site1 = WebTableParser()
 site1.create('https://calculador.com.br/tabela/indice/IGP-M', 'class',
              'table table-bordered table-striped table-hover table-fixed mb-0')
 table1 = site1.capture()
 df1 = site1.parse(table1)
-print(df1)
+print(df1)'''
 # Site 2 by webtableparserpreviousversion module.
 # You can not use it, it´s only a previous version
 '''site2 = WebTableParser('https://pt.wikipedia.org/wiki/\
@@ -20,11 +20,11 @@ table2 = site2.capture()
 df2 = site2.parse(table2)
 print(df2)'''
 # Site 3 capture and parsing
-'''site = WebTableParser()
+site = WebTableParser()
 site.create('https://www.fundsexplorer.com.br/ranking',
             'id', 'table-ranking')
 table = site.capture()
 df = site.parse(table)
-print(df)'''
+print(df)
 #
 # Now you have to process the dataframe, "clean and organize" it!
