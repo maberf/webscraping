d4b39eafcb0ca82b64f91d9e8b318db2c1508ea8
Maber Fernandes <71026050+maberf@users.noreply.github.com>
2023-09-17 12:26:52 -0300

    Update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # MAIN
 # packages urllib, beatiful soup, html5, pandas
 #
-# from webtableparserpreviousversion import WebTableParser
-from src.webtableparser import WebTableParser
+from src.webtableparserprevious import WebTableParser
+# from src.webtableparser import WebTableParser
 #
 # Sites 1 e 2 for testing (remove comments)
 '''site1 = WebTableParser()
@@ -13,18 +13,17 @@ df1 = site1.parse(table1)
 print(df1)'''
 # Site 2 by webtableparserpreviousversion module.
 # You can not use it, it´s only a previous version
-'''site2 = WebTableParser('https://pt.wikipedia.org/wiki/\
-    Lista_de_capitais_do_Brasil_por_%C3%A1rea',
+site2 = WebTableParser('https://pt.wikipedia.org/wiki/Lista_de_capitais_do_Brasil_por_%C3%A1rea',
                        'wikitable sortable')
 table2 = site2.capture()
 df2 = site2.parse(table2)
-print(df2)'''
+print(df2)
 # Site 3 capture and parsing
-site = WebTableParser()
+'''site = WebTableParser()
 site.create('https://www.fundsexplorer.com.br/ranking',
             'id', 'table-ranking')
 table = site.capture()
 df = site.parse(table)
-print(df)
+print(df)'''
 #
 # Now you have to process the dataframe, "clean and organize" it!
